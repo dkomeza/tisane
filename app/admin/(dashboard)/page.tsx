b8f17ec6e14f2aa3b8d5cd5a821395a01c66533c
Dawid Komęza <dawid.komeza@gmail.com>
2025-12-02 22:15:28 +0100

    🐛 Fix not checking authorization on admin dashboard

--- app/admin/(dashboard)/page.tsx
+++ app/admin/(dashboard)/page.tsx
@@ -1,4 +1,13 @@
-export default function AdminDashboard() {
+import { authorize } from "@/lib/auth/authorize";
+import { redirect } from "next/navigation";
+
+export default async function AdminDashboard() {
+  const { authorized } = await authorize();
+
+  if (!authorized) {
+    redirect("/admin/login");
+  }
+
   return (
     <div>
       <h1 className="text-2xl font-bold">Dashboard</h1>
