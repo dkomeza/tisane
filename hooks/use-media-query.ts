743d932c050a16608ab434173f3593612aaf2af7
Dawid Komęza <dawid.komeza@gmail.com>
2025-12-02 22:13:16 +0100

    🐛 Fix error during ssr

--- hooks/use-media-query.ts
+++ hooks/use-media-query.ts
@@ -3,6 +3,7 @@ import { useSyncExternalStore } from "react";
 export function useMediaQuery(query: string): boolean {
   return useSyncExternalStore(
     (onStoreChange) => {
+      if (typeof window === "undefined") return () => {};
       const mql = window.matchMedia(query);
       mql.addEventListener("change", onStoreChange);
       return () => mql.removeEventListener("change", onStoreChange);
