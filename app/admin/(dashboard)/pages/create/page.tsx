b1048715860e1ed5f0b5a14431c893a594014fd9
Magdalena Śmietana <ola75magda@gmail.com>
2025-12-27 10:12:23 +0100

    Edit Create PAge

--- app/admin/(dashboard)/pages/create/page.tsx
+++ app/admin/(dashboard)/pages/create/page.tsx
@@ -1,14 +1,31 @@
+"use client";
+
 import { PageForm } from "@/app/components/PageForm";
+import { createPage } from "../actions";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function CreatePage() {
+  const router = useRouter();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   return (
     <>
       <h1 className="text-2xl font-bold mb-6">Create Page</h1>
       <PageForm
+        isSubmitting={isSubmitting}
         onSubmit={async (data) => {
-          console.log("CREATE", data);
-          toast.success("Page created");
+          setIsSubmitting(true);
+          const result = await createPage(data);
+          setIsSubmitting(false);
+
+          if (result.error) {
+            toast.error(result.error);
+          } else {
+            toast.success("Page created");
+            router.push("/admin/pages");
+          }
         }}
       />
     </>
