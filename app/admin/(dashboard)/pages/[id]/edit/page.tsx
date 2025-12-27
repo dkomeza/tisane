170907899c0e68297a08d7bd614e3316b4df47e7
Magdalena Śmietana <ola75magda@gmail.com>
2025-12-27 09:29:04 +0100

    ✨ Add route /admin/pages/create and /admin/pages/[id]/edit

---
+++ app/admin/(dashboard)/pages/[id]/edit/page.tsx
@@ -0,0 +1,8 @@
+export default async function EditPage() {
+
+    return (
+        <div>
+            EditPage
+        </div>
+    );
+}
---
+++ app/admin/(dashboard)/pages/create/page.tsx
@@ -0,0 +1,8 @@
+export default async function CreatePage() {
+
+    return (
+        <div>
+            CreatePage
+        </div>
+    );
+}
