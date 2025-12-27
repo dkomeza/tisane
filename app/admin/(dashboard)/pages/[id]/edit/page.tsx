45cf77a227fb6f5605a5abd89ef82837d31fe46b
Magdalena Śmietana <ola75magda@gmail.com>
2025-12-27 09:54:59 +0100

    Connect PageForm to edit page

--- app/admin/(dashboard)/pages/[id]/edit/page.tsx
+++ app/admin/(dashboard)/pages/[id]/edit/page.tsx
@@ -1,8 +1,27 @@
-export default async function EditPage() {
+import { PageForm } from "@/app/components/PageForm";
+import { PageFormValues } from "@/lib/schemas/EditCreateSchema";
+import { toast } from "sonner";
 
-    return (
-        <div>
-            EditPage
-        </div>
-    );
+export default function EditPage() {
+  const mock: PageFormValues = {
+    title: "About",
+    slug: "about",
+    status: "published",
+    seoTitle: "About us",
+    seoDescription: "About company",
+    content: "{}",
+  };
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-6">Edit Page</h1>
+      <PageForm
+        defaultValues={mock}
+        onSubmit={async (data) => {
+          console.log("UPDATE", data);
+          toast.success("Page updated");
+        }}
+      />
+    </>
+  );
 }
