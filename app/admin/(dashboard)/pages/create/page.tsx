bd92cc6e745384b8673789cb94e45717bdf9ef7d
Magdalena Śmietana <ola75magda@gmail.com>
2025-12-27 09:47:25 +0100

    Connect PageForm to create page

--- app/admin/(dashboard)/pages/create/page.tsx
+++ app/admin/(dashboard)/pages/create/page.tsx
@@ -1,8 +1,16 @@
-export default async function CreatePage() {
+import { PageForm } from "@/app/components/PageForm";
+import { toast } from "sonner";
 
-    return (
-        <div>
-            CreatePage
-        </div>
-    );
+export default function CreatePage() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-6">Create Page</h1>
+      <PageForm
+        onSubmit={async (data) => {
+          console.log("CREATE", data);
+          toast.success("Page created");
+        }}
+      />
+    </>
+  );
 }
