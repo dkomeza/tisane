c962db3194c6a91635465dc24ef72c27ee63486f
Magdalena Śmietana <ola75magda@gmail.com>
2026-01-02 22:22:20 +0100

    ✨ Add admin form

--- components/registry/elements/Button.tsx
+++ components/registry/elements/Button.tsx
@@ -4,14 +4,16 @@ import {
   BlockProps,
   CMSComponent,
 } from "@/components/registry";
+import { cn } from "@/lib/utils";
 import { ArrowRight } from "lucide-react";
 import z from "zod";
+
+type Color = "primary" | "dark" | "white" | "violet" | "pink" | "clear";
 
 type ButtonProps = {
   content: string;
   variant: "small" | "large";
-  color: "primary" | "dark" | "white" | "violet" | "pink" | "clear";
-  textColor: "black" | "white" | "pink" | "violet";
+  color: Color;
   isDisabled: boolean;
 };
 
@@ -29,26 +31,19 @@ export const Button: CMSComponent<"button", ButtonProps> = {
     color: z
       .enum(["primary", "dark", "white", "violet", "pink", "clear"])
       .default("primary"),
-    textColor: z.enum(["black", "white", "pink", "violet"]).default("white"),
     isDisabled: z.boolean().default(false),
   }),
 };
 
 function ButtonClientComponent({ data }: BlockProps<ButtonProps>) {
   const colorStyles = {
-    primary: "bg-[#6B2AF6] text-white hover:bg-[#4f46e5]",
-    dark: "bg-[#8C1858] text-white hover:bg-black",
-    white: "bg-[#F8F8F8] text-black border border-gray-100 hover:bg-gray-50",
-    violet: "bg-[#9061F5] text-white hover:bg-[#7c3aed]",
-    pink: "bg-[#FF2B97] text-white hover:bg-[#db2777]",
-    clear: "bg-transparent text-black hover:bg-gray-100",
-  };
-
-  const textColorStyles = {
-    black: "text-black",
-    white: "text-white",
-    pink: "text-pink-500",
-    violet: "text-violet-500",
+    primary: "bg-[#6B2AF6] text-white hover:bg-[#4f46e5] disabled:bg-[#a1a1aa]",
+    dark: "bg-[#8C1858] text-white hover:bg-black disabled:bg-[#a1a1aa]",
+    white:
+      "bg-[#F8F8F8] text-black border border-gray-100 hover:bg-gray-50 disabled:bg-gray-200",
+    violet: "bg-[#9061F5] text-white hover:bg-[#7c3aed] disabled:bg-[#a1a1aa]",
+    pink: "bg-[#FF2B97] text-white hover:bg-[#db2777] disabled:bg-[#a1a1aa]",
+    clear: "bg-transparent text-black hover:bg-gray-100 disabled:bg-gray-200",
   };
 
   const sizeStyles = {
@@ -59,28 +54,20 @@ function ButtonClientComponent({ data }: BlockProps<ButtonProps>) {
   const disabledStyle =
     "bg-[#a1a1aa] text-white/70 cursor-not-allowed opacity-50 pointer-events-none";
 
-  const finalClass = data.isDisabled
-    ? disabledStyle
-    : colorStyles[data.color] +
-      " " +
-      textColorStyles[data.textColor] +
-      " " +
-      sizeStyles[data.variant];
+  const finalClass = colorStyles[data.color] + " " + sizeStyles[data.variant];
 
   return (
     <button
       disabled={data.isDisabled}
-      className={`
-        flex items-center justify-center gap-3 px-8 py-3 
-        font-medium transition-colors duration-200 
-        ${finalClass}
-      `}
+      className={cn(
+        "flex items-center justify-center gap-3 font-medium transition-colors duration-200 disabled:pointer-events-none",
+        colorStyles[data.color],
+        sizeStyles[data.variant]
+      )}
     >
-      <span>
-        <ArrowRight className="w-4 h-4" /> 
-        {data.content}
-        <ArrowRight className="w-4 h-4" />
-      </span>
+      <ArrowRight className="" />
+      <span className="flex-1 text-center">{data.content}</span>
+      <ArrowRight className="w-10 h-10" />
     </button>
   );
 }
@@ -93,10 +80,44 @@ function ButtonAdminComponent({ id, useStore }: AdminBlockProps<ButtonProps>) {
   if (!block) return null;
 
   return (
-    <textarea
-      value={block.data.content}
-      onChange={(e) => updateBlock(id, { content: e.target.value })}
-    ></textarea>
+    <div className="flex flex-col gap-2">
+      <textarea
+        value={block.data.content}
+        onChange={(e) => updateBlock(id, { content: e.target.value })}
+      ></textarea>
+      <select
+        name="color"
+        id="color"
+        value={block.data.color}
+        onChange={(e) => updateBlock(id, { color: e.target.value as Color })}
+      >
+        <option value="primary">Primary</option>
+        <option value="dark">Dark</option>
+        <option value="white">White</option>
+        <option value="violet">Violet</option>
+        <option value="pink">Pink</option>
+        <option value="clear">Clear</option>
+      </select>
+
+      <input
+        type="checkbox"
+        checked={block.data.isDisabled}
+        onChange={(e) => updateBlock(id, { isDisabled: e.target.checked })}
+      />
+      <label htmlFor="isDisabled">Is Disabled</label>
+
+      <select
+        name="variant"
+        id="variant"
+        value={block.data.variant}
+        onChange={(e) =>
+          updateBlock(id, { variant: e.target.value as "small" | "large" })
+        }
+      >
+        <option value="small">Small</option>
+        <option value="large">Large</option>
+      </select>
+    </div>
   );
 }
 
