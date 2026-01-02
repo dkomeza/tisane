11d42f12f3cc16f95343d0badda74d80bd488d34
Dawid Komęza <dawid.komeza@gmail.com>
2026-01-02 23:07:06 +0100

    ✨ Add component rendering

--- components/registry/index.ts
+++ components/registry/index.ts
@@ -32,16 +32,46 @@ export type CMSComponent<Id extends string, Props> = {
 };
 
 /**
+ * A DBComponent represents an instance of a component stored in the database.
+ * It includes the component's type (ID), data adhering to the component's schema,
+ * and optionally an array of child components for nested structures.
+ */
+export interface DBComponent<T extends ComponentType = ComponentType> {
+  type: T;
+  data: z.infer<ComponentRegistry[T]["Schema"]>;
+  children?: DBComponent[];
+}
+
+/**
+ * The DBComponentSchema is a recursive Zod schema that validates
+ * DBComponent structures, including nested children.
+ */
+export const DBComponentSchema: z.ZodType<DBComponent> = z.lazy(() => {
+  const options = Object.entries(COMPONENT_REGISTRY).map(([key, value]) => {
+    return z.object({
+      type: z.literal(key),
+      data: value.Schema,
+      children: z.array(DBComponentSchema).optional(),
+    });
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return z.discriminatedUnion("type", options as any);
+});
+
+export const DBComponentsArraySchema = z.array(DBComponentSchema);
+
+/**
  * A Block represents an instance of a component in the Zustand store.
  * Each block has a unique ID, a type corresponding to a component in the registry,
  * and data that adheres to the schema defined by that component.
  */
-export type Block<T extends ComponentType = ComponentType> = {
+export interface Block<
+  T extends ComponentType = ComponentType,
+> extends DBComponent<T> {
   id: string;
-  type: string;
-  data: z.infer<ComponentRegistry[T]["Schema"]>;
   children?: Block[];
-};
+}
 
 /**
  * The COMPONENT_REGISTRY is a centralized registry of all available CMS components.
