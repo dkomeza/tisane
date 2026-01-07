141fcddb9e141830ccbcda7dc515f117c7ca67b3
Magdalena Śmietana <ola75magda@gmail.com>
2026-01-07 10:52:41 +0100

    💄 Cosmetic gaps

--- components/registry/elements/underlined-card/UnderlinedCardClient.tsx
+++ components/registry/elements/underlined-card/UnderlinedCardClient.tsx
@@ -10,13 +10,13 @@ export function UnderlinedCardClient({
 }: BlockProps<UnderlinedCardProps>) {
   return (
     <div
-      className="w-full border-b"
+      className="w-full border-b flex flex-col gap-6 pb-4"
       style={{
         borderBottomColor: data.underlineColor,
         width: data.width,
       }}
     >
-      <div className="overflow-hidden flex items-center justify-center gap-24">
+      <div className="overflow-hidden flex">
         {data.mediaId ? (
           <ImageClient data={{ mediaId: data.mediaId }} id={`${id}-image`} />
         ) : (
@@ -27,7 +27,7 @@ export function UnderlinedCardClient({
       </div>
 
       {/* Text Part & Description */}
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
         <div className="text-[26px] text-[#D2CFCB]">{data.text}</div>
         <div className="text-[16px] text-[#D2CFCB]">{data.description}</div>
       </div>
