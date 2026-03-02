df29f7cb2a7587409aba51d812cb224bd7df6001
Dawid Komęza <dawid.komeza@gmail.com>
2026-03-02 21:01:23 +0100

    🐛 Fix local next images

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,27 @@
 import type { NextConfig } from "next";
+import { RemotePattern } from "next/dist/shared/lib/image-config";
 const development = process.env.NODE_ENV !== "production";
+
+const s3Endpoint = process.env.S3_PUBLIC_ENDPOINT
+  ? new URL(process.env.S3_PUBLIC_ENDPOINT)
+  : null;
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
   output: "standalone",
   images: {
     dangerouslyAllowLocalIP: development,
+    remotePatterns:
+      development && s3Endpoint
+        ? ([
+            {
+              protocol: s3Endpoint.protocol.replace(":", ""),
+              hostname: s3Endpoint.hostname,
+              port: s3Endpoint.port,
+              pathname: "/**",
+            },
+          ] as RemotePattern[])
+        : undefined,
   },
 };
 
