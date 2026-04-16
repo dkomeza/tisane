a64fcc762b2cb862523506f0f0a830e9a9ae3c22
Dawid Komęza <dawid.komeza@gmail.com>
2026-04-16 19:08:09 +0200

    feat: add itday plugin and refactor SignupSchema to support token-less onboarding schemas

--- lib/schemas/OnboardingSchema.ts
+++ lib/schemas/OnboardingSchema.ts
@@ -1,9 +1,7 @@
-import { SignupSchema } from "./SignupSchema";
+import { BaseSignupSchema } from "./SignupSchema";
 import { z } from "zod";
 
-export const OnboardingSchema = SignupSchema.omit({
-  token: true,
-}).safeExtend({
+export const OnboardingSchema = BaseSignupSchema.safeExtend({
   email: z.email("Please enter a valid email address"),
   siteUrl: z.url("Please enter a valid URL"),
 });
